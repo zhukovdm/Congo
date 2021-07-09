bcce09ce1652dde5360b2d6a2eb1de00fa815e3c
Dmitry Zhukov <zhukov.dm.s@gmail.com>
2021-07-09 23:29:37 +0200

    Command line interface and specific command line configuration.

---
+++ Congo.CLI/CommandLineConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+
+using Congo.Def;
+
+namespace Congo.CLI {
+
+	class CongoCommandLineConfiguration : ICommandLineConfiguration {
+
+		private static readonly string greetView =
+			File.ReadAllText("Resources\\CLI\\Congo\\greet.txt");
+
+		private static readonly ImmutableDictionary<PieceCode, string> pieceView =
+			new Dictionary<PieceCode, string>() {
+				{ PieceCode.Lion, " l" }, { PieceCode.Zebra, " z" },
+				{ PieceCode.Elephant, " e" }, { PieceCode.Giraffe, " g" },
+				{ PieceCode.Crocodile, " c" }, { PieceCode.Pawn, " p" },
+				{ PieceCode.Superpawn, " s" }, { PieceCode.Monkey, " m" },
+				{ PieceCode.Empty, " -" }, { PieceCode.Captured, " x" }
+			}.ToImmutableDictionary();
+
+		private static readonly CongoCommandLineConfiguration clv =
+			new CongoCommandLineConfiguration();
+		
+		public static ICommandLineConfiguration GetInstance() => clv;
+
+		private CongoCommandLineConfiguration() { }
+
+		public string GreetView { get => greetView; }
+		public ImmutableDictionary<PieceCode, string> PieceView { get => pieceView; }
+
+	}
+
+}
---
+++ Congo.CLI/CommandLineInterface.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+
+using Congo.Def;
+
+namespace Congo.CLI {
+
+	public sealed class CommandLineInterface : IUserInterface {
+
+		readonly private static ImmutableDictionary<GameCode, ICommandLineConfiguration> configs =
+			new Dictionary<GameCode, ICommandLineConfiguration>() {
+				{ GameCode.Congo, CongoCommandLineConfiguration.GetInstance() }
+			}.ToImmutableDictionary();
+		
+		readonly private static TextReader reader = Console.In;
+		readonly private static TextWriter writer = Console.Out;
+
+		public static IUserInterface GetParametrizedInstance(GameCode gameCode) =>
+			new CommandLineInterface(gameCode);
+
+		private readonly ICommandLineConfiguration config;
+
+		private CommandLineInterface(GameCode gameCode) {
+			if (!configs.TryGetValue(gameCode, out config)) {
+				throw new Exception($"Game with code {gameCode} does not exist.");
+			}
+		}
+
+		public void Greet() { writer.WriteLine(config.GreetView); }
+
+		public void ShowBoard(IBoard board) {
+			for (int rank = 0; rank < board.Size; rank++) {
+				writer.Write($" {board.Size - rank} ");
+				for (int file = 0; file < board.Size; file++) {
+					var pv = config.PieceView[board.GetPieceCode(rank, file)];
+					if (board.IsFirstMovePiece(rank, file)) pv = pv.ToUpper();
+					writer.Write(pv);
+				}
+				writer.WriteLine();
+			}
+			writer.WriteLine();
+			writer.Write(" / ");
+			for (int i = 0; i < board.Size; i++) {
+				writer.Write(" " + ((char)('A' + i)).ToString());
+			}
+			writer.WriteLine();
+		}
+
+	}
+
+}
