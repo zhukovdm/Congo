8ab95b68e62746d0b9cd64f8659bc4240a310462
Dmitry Zhukov <zhukov.dm.s@gmail.com>
2021-07-09 23:32:18 +0200

    User Interface Factory, will provide different graphical user interfaces (and WFA).

---
+++ Congo.UI/UserInterfaceFactory.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+using Congo.CLI;
+using Congo.Def;
+
+namespace Congo.UI {
+
+	public static class UserInterfaceFactory {
+
+		private delegate IUserInterface D(GameCode gc);
+
+		private static Dictionary<UICode, D> delegateTable =
+			new Dictionary<UICode, D>() {
+				{
+					UICode.CommandLineInterface,
+					new D(CommandLineInterface.GetParametrizedInstance)
+				}
+			};
+
+		public static IUserInterface GetInstance(UICode uiCode, GameCode gameCode) {
+			return delegateTable[uiCode].Invoke(gameCode);
+		}
+	}
+
+}
