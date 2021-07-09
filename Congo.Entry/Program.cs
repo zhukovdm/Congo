2552f64151ae0787406ea183d70034ffeb9e9f5e
Dmitry Zhukov <zhukov.dm.s@gmail.com>
2021-07-09 23:30:35 +0200

    Entry project for easier project dependency handling (might be changed in the future).

---
+++ Congo.Entry/Program.cs
@@ -0,0 +1,23 @@
+﻿using Congo.Core;
+using Congo.Def;
+
+namespace Congo.Entry {
+	class Program {
+		static void Main(string[] args) {
+			IGame game = CongoGame.GetStandard(UICode.CommandLineInterface);
+
+			/*/
+			bool last;
+			do {
+				IGame game = CongoGame.GetStandard(UIType.CommandLineInterface);
+				game.Show();
+				while (game.InProgress()) {
+					game = game.MakeProgress();
+					game.Show();
+				}
+				last = game.IsLast();
+			} while (!last);
+			/**/
+		}
+	}
+}
