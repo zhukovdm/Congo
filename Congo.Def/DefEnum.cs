c9fa0dfae937963024c7a466cd71c5ba60754fb6
Dmitry Zhukov <zhukov.dm.s@gmail.com>
2021-07-09 23:26:54 +0200

    Project for high level definitions.

---
+++ Congo.Def/DefEnum.cs
@@ -0,0 +1,20 @@
+﻿namespace Congo.Def {
+    
+    public enum ColorCode : int {
+		White, Black
+	}
+
+	public enum PieceCode : uint {
+		Empty, Lion, Zebra, Elephant, Giraffe,
+		Crocodile, Pawn, Superpawn, Monkey, Captured
+	}
+
+	public enum UICode {
+		CommandLineInterface
+	}
+
+	public enum GameCode {
+		Congo
+	}
+
+}
---
+++ Congo.Def/DefInterface.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Immutable;
+
+namespace Congo.Def {
+
+	public interface IUserInterface {
+		void Greet();
+		void ShowBoard(IBoard board);
+	}
+
+	public interface IGame {
+		void Show();
+		bool InProgress();
+		IGame MakeProgress();
+		void ReportResult();
+		bool Repeat();
+	}
+
+	public interface IBoard {
+		int Size { get; }
+		PieceCode GetPieceCode(int rank, int file);
+		bool IsFirstMovePiece(int rank, int file);
+	}
+
+	public interface ICommandLineConfiguration {
+		string GreetView { get; }
+		ImmutableDictionary<PieceCode, string> PieceView { get; }
+	}
+
+}
