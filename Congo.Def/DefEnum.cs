4edbef6e9e4e014d074e2e0096fe5e31b7938bc4
Dmitry Zhukov <zhukov.dm.s@gmail.com>
2021-07-11 17:17:24 +0200

    Position code in case direct square reference is necessary within the code.

--- Congo.Def/DefEnum.cs
+++ Congo.Def/DefEnum.cs
@@ -1,6 +1,16 @@
 ﻿namespace Congo.Def {
     
-    public enum ColorCode : int {
+	public enum PositionCode : int {
+		A7, B7, C7, D7, E7, F7, G7,
+		A6, B6, C6, D6, E6, F6, G6,
+		A5, B5, C5, D5, E5, F5, G5,
+		A4, B4, C4, D4, E4, F4, G4,
+		A3, B3, C3, D3, E3, F3, G3,
+		A2, B2, C2, D2, E2, F2, G2,
+		A1, B1, C1, D1, E1, F1, G1
+	}
+
+	public enum ColorCode : int {
 		White, Black
 	}
 
@@ -9,11 +19,11 @@
 		Crocodile, Pawn, Superpawn, Monkey, Captured
 	}
 
-	public enum UICode {
+	public enum UICode : int {
 		CommandLineInterface
 	}
 
-	public enum GameCode {
+	public enum GameCode : int {
 		Congo
 	}
 
