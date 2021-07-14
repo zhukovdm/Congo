b9b938fded222e0ca4ebcef6dd0f52fa4878c34d
Dmitry Zhukov <zhukov.dm.s@gmail.com>
2021-07-14 19:24:55 +0200

    Decoder test instead of Player

--- Congo.Core.MSTest/DecoderTest.cs
+++ Congo.Core.MSTest/DecoderTest.cs
@@ -3,14 +3,14 @@
 namespace Congo.Core.MSTest {
 
 	[TestClass]
-	public class PlayerTest {
+	public class DecoderTest {
 
 		[TestMethod]
-		public void Player_DeBruijnLSB() {
+		public void Decoder_DeBruijnLSB() {
 			var result = true;
 			var num = 0x1UL;
 			for (int i = 0; i < 64; i++) {
-				var immed = CongoPlayer.DeBruijnLSB(num << i);
+				var immed = Decoder.DeBruijnLSB(num << i);
 				result &= immed == i;
 			}
 			Assert.IsTrue(result);
