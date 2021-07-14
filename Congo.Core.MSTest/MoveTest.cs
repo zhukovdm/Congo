835d66a8f9d73ec6b263bb1a4d15ad3bdc4f7a12
Dmitry Zhukov <zhukov.dm.s@gmail.com>
2021-07-14 23:15:43 +0200

    Move comparer test.

---
+++ Congo.Core.MSTest/MoveTest.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Congo.Def;
+
+namespace Congo.Core.MSTest {
+
+	[TestClass]
+	public class Move_Test {
+
+		[TestMethod]
+		public void StaticComparer_LessThanByFr() {
+			var m1 = new CongoMove((int)SquareCode.A7, (int)SquareCode.C7);
+			var m2 = new CongoMove((int)SquareCode.B7, (int)SquareCode.B7);
+			var actual = CongoMove.Comparer(m1, m2); // A7 < B7 & C7 > B7
+			int expected = -1;
+			Assert.IsTrue(expected == actual);
+		}
+
+		[TestMethod]
+		public void StaticComparer_LessThanByTo() {
+			var m1 = new CongoMove((int)SquareCode.A7, (int)SquareCode.B7);
+			var m2 = new CongoMove((int)SquareCode.A7, (int)SquareCode.C7);
+			var actual = CongoMove.Comparer(m1, m2); // A7 == A7 & B7 < C7
+			int expected = -1;
+			Assert.IsTrue(expected == actual);
+		}
+
+		[TestMethod]
+		public void StaticComparer_Equals() {
+			var m1 = new CongoMove((int)SquareCode.A7, (int)SquareCode.B7);
+			var m2 = new CongoMove((int)SquareCode.A7, (int)SquareCode.B7);
+			var actual = CongoMove.Comparer(m1, m2);
+			int expected = 0;
+			Assert.IsTrue(expected == actual);
+		}
+
+		[TestMethod]
+		public void StaticComparer_GreaterThanByFr() {
+			var m1 = new CongoMove((int)SquareCode.B7, (int)SquareCode.B7);
+			var m2 = new CongoMove((int)SquareCode.A7, (int)SquareCode.C7);
+			var actual = CongoMove.Comparer(m1, m2); // B7 > A7 & B7 < C7
+			int expected = 1;
+			Assert.IsTrue(expected == actual);
+		}
+
+		[TestMethod]
+		public void StaticComparer_GreaterThanByTo() {
+			var m1 = new CongoMove((int)SquareCode.A7, (int)SquareCode.E4);
+			var m2 = new CongoMove((int)SquareCode.A7, (int)SquareCode.D4);
+			var actual = CongoMove.Comparer(m1, m2); // A7 == A7 & D5 > D4
+			int expected = 1;
+			Assert.IsTrue(expected == actual);
+		}
+
+	}
+}
