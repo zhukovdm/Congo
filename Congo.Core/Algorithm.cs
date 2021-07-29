f124bfc989c4efa544a2ab993e93a5787c7f399d
Dmitry Zhukov <zhukov.dm.s@gmail.com>
2021-07-29 23:43:58 +0200

    4th negamax draft, method returns (score, move) tuple.

--- Congo.Core/Algorithm.cs
+++ Congo.Core/Algorithm.cs
@@ -14,86 +14,58 @@ namespace Congo.Core
 			return game.ActivePlayer.Moves[rnd.Next(upperBound)];
 		}
 
-		private struct Eval
-		{
-			public readonly int Score;
-			public readonly CongoMove Move;
+		private static (int, CongoMove) Max((int, CongoMove) pairA, (int, CongoMove) pairB)
+			=> pairA.Item1 >= pairB.Item1 ? pairA : pairB;
 
-			public Eval(int score, CongoMove move) { Score = score; Move = move; }
-
-			public Eval With(int score) => new Eval(score, Move);
-		}
-
-		private static Eval Max(Eval e1, Eval e2) => e1.Score >= e2.Score ? e1 : e2;
-
-		/*
-		private static int negamaxGetScore(CongoGame game, int alpha, int beta, int depth)
-		{
-			int score = -Evaluator.INF;
-
-			if (game.HasEnded() || depth == 0) {
-				score = Evaluator.Material(game);
-				return game.Opponent.Color.IsWhite() ? score : -score;
-			}
-
-			var moves = game.ActivePlayer.Moves;
-			for (int i = 0; i < moves.Length; i++) {
-				var newGame = game.Transition(moves[i]);
-				score = Math.Max(score, negamaxGetScore(newGame, -beta, -alpha, depth - 1));
-
-				// fail-hard beta cut-off, no opponent's alpha update (--beta = alpha)
-				if (score >= beta) { return -beta; }
-
-				// alpha update
-				if (score > alpha) { alpha = score; }
-			}
-
-			// the better for the active player -> the worser for its opponent
-			return -score;
-		}
-		*/
-
-		private static Eval negamaxSingleThread(CongoGame game,
+		/* In order to simplify source code, this method combines both,
+		 * searching best score and returning best move. This is the reason 
+		 * why we return null or pass null to the call, or ignore returned 
+		 * move. */
+		private static (int, CongoMove) negamaxSingleThread(CongoGame game,
 			ImmutableArray<CongoMove> moves, int alpha, int beta, int depth)
 		{
-			var eval = new Eval(-Evaluator.INF, null);
-			
+			int score = -Evaluator.INF;
+			CongoMove move = null;
+
 			// recursion bottom
 			if (game.HasEnded() || depth <= 0) {
-				var score = game.Predecessor.ActivePlayer.Color.IsWhite()
+				score = game.Predecessor.ActivePlayer.Color.IsWhite()
 					? Evaluator.Material(game)
 					: -Evaluator.Material(game);
 
-				return new Eval(score, game.TransitionMove);
+				return (score, null); // predecessor knows moves[i]
 			}
 
+			// 
 			for (int i = 0; i < moves.Length; i++) {
 				var newGame = game.Transition(moves[i]);
 
 				// ordinary move, active player changes the color
 				if (newGame.ActivePlayer.Color != game.ActivePlayer.Color) {
-					eval = Max(eval, negamaxSingleThread(
+					(score, _) = Max((score, null), negamaxSingleThread(
 						newGame, newGame.ActivePlayer.Moves, -beta, -alpha, depth - 1));
 				}
 
 				// multiple monkey jump, no color change
 				else {
-					eval = Max(eval, negamaxSingleThread(
+					(score, _) = Max((score, null), negamaxSingleThread(
 						newGame, newGame.ActivePlayer.Moves, alpha, beta, depth - 1));
 				}
 
 				// fail-hard beta cut-off
-				if (eval.Score >= beta) { eval = eval.With(beta); break; }
+				if (score >= beta) { score = beta; break; }
 
 				// alpha update
-				if (eval.Score > alpha) { alpha = eval.Score; }
+				if (score > alpha) { alpha = score; move = moves[i]; }
 			}
 
-			// root game or monkey jump
+			/* root game (no predecessor) or monkey jump (predecessor's
+			 * active player is of the same color) */
+
 			var condition = depth == negamaxDepth ||
 				game.Predecessor.ActivePlayer.Color == game.ActivePlayer.Color;
 
-			return condition ? eval : eval.With(-eval.Score);
+			return condition ? (score, move) : (-score, move);
 		}
 
 		private static CongoMove negamaxMultiThread(CongoGame game, int depth)
@@ -101,7 +73,7 @@ namespace Congo.Core
 			return null;
 		}
 
-		private static readonly int negamaxDepth = 1;
+		private static readonly int negamaxDepth = 5;
 		
 		public static CongoMove Negamax(CongoGame game)
 		{
@@ -111,10 +83,10 @@ namespace Congo.Core
 
 			if (game.HasEnded() || negamaxDepth <= 0) { return null; }
 
-			var eval = negamaxSingleThread(game, game.ActivePlayer.Moves,
+			var (_, move) = negamaxSingleThread(game, game.ActivePlayer.Moves,
 				-Evaluator.INF, Evaluator.INF, negamaxDepth);
 			
-			return eval.Move;
+			return move;
 		}
 
 		public static CongoMove IterDeep(CongoGame game) => Rnd(game);
