8a7c79e4f4b088c784fa85ceb746ba1228f56a2a
Dmitry Zhukov <zhukov.dm.s@gmail.com>
2022-05-13 19:46:33 +0200

    separate IParametrizedEnum

--- Congo.Core/Board.cs
+++ Congo.Core/Board.cs
@@ -5,17 +5,6 @@ using System.Runtime.CompilerServices;
 
 namespace Congo.Core
 {
-    public interface IParametrizedEnumerator<T>
-    {
-        T Current { get; }
-        bool MoveNext();
-    }
-
-    public interface IParametrizedEnumerable<T, U>
-    {
-        IParametrizedEnumerator<U> GetEnumerator(T param);
-    }
-
     public sealed class CongoBoard : IParametrizedEnumerable<CongoColor, int>
     {
         private static readonly int size = 7;
---
+++ Congo.Core/IParametrizedEnum.cs
@@ -0,0 +1,13 @@
+﻿namespace Congo.Core
+{
+    public interface IParametrizedEnumerator<T>
+    {
+        T Current { get; }
+        bool MoveNext();
+    }
+
+    public interface IParametrizedEnumerable<T, U>
+    {
+        IParametrizedEnumerator<U> GetEnumerator(T param);
+    }
+}
