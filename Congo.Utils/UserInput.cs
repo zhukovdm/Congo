dddc6c36104a4e293d84ee8f47050ac34714a186
Dmitry Zhukov <zhukov.dm.s@gmail.com>
2022-05-13 13:09:56 +0200

    init UserInput in Congo.Utils

---
+++ Congo.Utils/UserInput.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+
+namespace Congo.Utils
+{
+    public class UserInput
+    {
+        public static bool IsUserNameValid(string name)
+        {
+            return name.Length > 0
+                && name.All(char.IsLetterOrDigit);
+        }
+
+        public static bool IsIpAddressHolderValid(string holder)
+        {
+            return holder.Length > 0
+                && holder.Length < 4
+                && holder.All(char.IsDigit)
+                && int.TryParse(holder, out var result)
+                && result >= 0
+                && result <= 255;
+        }
+
+        public static bool IsPortAddressValid(string port)
+        {
+            return port.Length == 5
+                && port.All(char.IsDigit)
+                && int.TryParse(port, out var result)
+                && result >= 49152
+                && result <= 65535;
+        }
+    }
+}
