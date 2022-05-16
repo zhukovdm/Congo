36fed3d7324a1b3d13b7e27847fec63713092988
Dmitry Zhukov <zhukov.dm.s@gmail.com>
2022-05-16 15:28:00 +0200

    remove `Test`, create `Congo.Test`

---
+++ Congo.Test/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Congo.Test
+{
+    internal class Program
+    {
+        static void Main(string[] _)
+        {
+        }
+    }
+}
--- Congo.Test/Properties/AssemblyInfo.cs
+++ Congo.Test/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Test")]
+[assembly: AssemblyTitle("Congo.Test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Test")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyProduct("Congo.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("edc54dec-2880-4cda-a457-518d2e270c32")]
+[assembly: Guid("11982fc4-4583-4a01-895a-20228722696c")]
 
 // Version information for an assembly consists of the following four values:
 //
