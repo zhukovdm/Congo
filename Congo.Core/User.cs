a7daef07209956726ce5fb09f7b8bbe43dadc116
Dmitry Zhukov <zhukov.dm.s@gmail.com>
2022-07-08 10:28:03 +0200

    add `Net` user

--- Congo.Core/User.cs
+++ Congo.Core/User.cs
@@ -3,22 +3,27 @@
     public abstract class CongoUser
     {
         protected AlgorithmDelegate algo;
-        public CongoMove Advise(CongoGame game) => algo.Invoke(game);
+
+        public CongoUser(AlgorithmDelegate algo)
+        {
+            this.algo = algo;
+        }
+
+        public virtual CongoMove Advise(CongoGame game) => algo.Invoke(game);
     }
 
     public sealed class Ai : CongoUser
     {
-        public Ai(AlgorithmDelegate algo)
-        {
-            this.algo = algo;
-        }
+        public Ai(AlgorithmDelegate algo) : base(algo) { }
     }
 
     public sealed class Hi : CongoUser
     {
-        public Hi(AlgorithmDelegate algo)
-        {
-            this.algo = algo;
-        }
+        public Hi(AlgorithmDelegate algo) : base(algo) { }
+    }
+
+    public sealed class Net : CongoUser
+    {
+        public Net(AlgorithmDelegate algo) : base(algo) { }
     }
 }
