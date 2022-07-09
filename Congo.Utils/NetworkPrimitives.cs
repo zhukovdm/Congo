f3d1825003734b852cf9bc55f4131995214aae45
Dmitry Zhukov <zhukov.dm.s@gmail.com>
2022-07-10 01:36:19 +0200

    utils network primitives

---
+++ Congo.Utils/NetworkPrimitives.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http;
+using Grpc.Net.Client;
+
+namespace Congo.Utils
+{
+    public static class NetworkPrimitives
+    {
+        public static GrpcChannel CreateRpcChannel(string host, string port)
+        {
+            // currently, ssl certificate is not supported!
+            var httpHandler = new HttpClientHandler
+            {
+                ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+            };
+
+            return GrpcChannel.ForAddress(
+                "https://" + host + ":" + port,
+                new GrpcChannelOptions { HttpHandler = httpHandler }
+            );
+        }
+    }
+}
