75736752340ca03ee703a59edc8418f5b22f10c1
Dmitry Zhukov <zhukov.dm.s@gmail.com>
2022-07-14 23:52:11 +0200

    gui grpc routines partial impl.

---
+++ Congo.GUI/GrpcRoutines.cs
@@ -0,0 +1,16 @@
+﻿using Congo.Server;
+
+namespace Congo.GUI
+{
+    internal static class GrpcRoutines
+    {
+        public static long PostFen(CongoGrpc.CongoGrpcClient client, string fen)
+            => client.PostFen(new PostFenRequest() { Fen = fen }).GameId;
+
+        public static bool CheckGameId(CongoGrpc.CongoGrpcClient client, long gameId)
+            => client.CheckGameId(new CheckGameIdRequest() { GameId = gameId }).Exist;
+
+        public static string GetFirstFen(CongoGrpc.CongoGrpcClient client, long gameId)
+            => client.GetFirstFen(new GetFirstFenRequest() { GameId = gameId }).Fen;
+    }
+}
