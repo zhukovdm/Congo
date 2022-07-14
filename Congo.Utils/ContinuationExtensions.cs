eec8f63ae3cfc8b6a93d7e915157743fd9f76227
Dmitry Zhukov <zhukov.dm.s@gmail.com>
2022-07-14 23:55:18 +0200

    continuation extension `AndThen`

---
+++ Congo.Utils/ContinuationExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Congo.Utils
+{
+    public static class ContinuationExtensions
+    {
+        /// <summary>
+        /// Left-associative ?? operator.
+        /// </summary>
+        public static T AndThen<T>(this T t, Func<T, T> func)
+            => t ?? func(t);
+    }
+}
