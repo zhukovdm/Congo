9b3d27fe67dec0d9ef94a977cb742cd6a19a528f
Dmitry Zhukov <zhukov.dm.s@gmail.com>
2022-07-15 23:58:29 +0200

    identical cli and gui grpc routines

--- Congo.CLI/GrpcRoutines.cs
+++ Congo.CLI/GrpcRoutines.cs
@@ -2,7 +2,6 @@
 using Congo.Server;
 using Congo.Utils;
 using System.Collections.Generic;
-using System.Linq;
 
 using CongoClient = Congo.Server.CongoGrpc.CongoGrpcClient;
 
@@ -10,6 +9,17 @@ namespace Congo.CLI
 {
     internal static class GrpcRoutines
     {
+        private static string getFirstFen(CongoClient client, long gameId)
+        {
+            var fen = client.GetFirstFen(new GetFirstFenRequest() { GameId = gameId }).Fen;
+
+            if (fen == string.Empty) {
+                throw new CongoServerResponseException($"First fen for gameId {gameId} cannot be obtained.");
+            }
+
+            return fen;
+        }
+
         private static string getLatestFen(CongoClient client, long gameId)
         {
             var fen = client.GetLatestFen(new GetLatestFenRequest() { GameId = gameId }).Fen;
@@ -47,27 +57,20 @@ namespace Congo.CLI
             return newId;
         }
 
-        public static bool CheckGameId(CongoClient client, long gameId)
-            => client.CheckGameId(new CheckGameIdRequest() { GameId = gameId }).Exist;
-
-        public static string GetFirstFen(CongoClient client, long gameId)
+        public static void ConfirmGameId(CongoClient client, long gameId)
         {
-            var fen = client.GetFirstFen(new GetFirstFenRequest() { GameId = gameId }).Fen;
-
-            if (fen == string.Empty) {
-                throw new CongoServerResponseException($"First fen for gameId {gameId} cannot be obtained.");
+            if (!client.CheckGameId(new CheckGameIdRequest() { GameId = gameId }).Exist) {
+                throw new CongoServerResponseException($"Game with {gameId} does not exist.");
             }
-
-            return fen;
         }
+
+        public static CongoGame GetFirstGame(CongoClient client, long gameId)
+            => CongoFen.FromFen(getFirstFen(client, gameId));
 
         public static CongoGame GetLatestGame(CongoClient client, long gameId)
             => CongoFen.FromFen(getLatestFen(client, gameId));
 
-        public static IEnumerable<CongoMove> GetMovesAfter(CongoClient client, long gameId, long moveId)
-        {
-            return from move in client.GetDbMovesAfter(new GetDbMovesAfterRequest() { GameId = gameId, MoveId = moveId }).Moves
-                   select new CongoMove(move.Fr, move.To);
-        }
+        public static ICollection<DbMove> GetDbMovesAfter(CongoClient client, long gameId, long moveId)
+            => client.GetDbMovesAfter(new GetDbMovesAfterRequest() { GameId = gameId, MoveId = moveId }).Moves;
     }
 }
