1c16b20f86eb346b8d988180c64475c63af0a6de
Dmitry Zhukov <zhukov.dm.s@gmail.com>
2022-07-15 16:53:19 +0200

    async job with abandon func.

--- Congo.GUI/AsyncJob.cs
+++ Congo.GUI/AsyncJob.cs
@@ -1,4 +1,6 @@
 ﻿using Congo.Core;
+using Grpc.Core;
+using System.Collections.Generic;
 using System.Threading;
 using System.Windows.Controls;
 
@@ -30,42 +32,89 @@ namespace Congo.GUI
         public abstract AsyncJob Run();
 
         public abstract void Cancel();
+
+        public abstract void Abandon();
     }
 
     internal sealed class AsyncAdvise : AsyncJob
     {
         private readonly bool sleep;
-        private readonly CongoUser User;
+        private readonly CongoUser user;
 
-        public readonly CongoGame Game;
+        public CongoGame Game { get; private set; }
+
         public CongoMove Move { get; private set; }
 
         public AsyncAdvise(CongoGame game, CongoUser user, bool sleep)
         {
             Game = game;
-            User = user;
+            this.user = user;
             this.sleep = sleep;
         }
 
         public override AsyncJob Run()
         {
             pauseEvent.Wait();
-            if (sleep) { Thread.Sleep(1000); } // avoid too fast user switching
+            if (sleep) { Thread.Sleep(1000); } // helps to avoid too fast ai-ai switching
 
-            Move = User.Advise(Game);
+            Move = user.Advise(Game);
 
             return this;
         }
 
         public override void Cancel() => Algorithm.Cancel();
+
+        public override void Abandon() => Algorithm.Cancel();
     }
 
-    /*
     internal sealed class AsyncNetMove : AsyncJob
     {
-        private volatile bool cancel = false;
+        private volatile bool abandon;
+        private readonly CongoMove move;
+        private readonly CongoUser white, black;
 
-        public AsyncNetMove() { }
+        public CongoGame Game { get; private set; }
+
+        public NetPack NetPack { get; private set; }
+
+        public IEnumerable<CongoMove> Moves { get; private set; }
+
+        public StatusCode StatusCode { get; private set; }
+
+        public AsyncNetMove(NetPack netPack, CongoMove move, CongoUser white, CongoUser black)
+        {
+            abandon = false;
+            this.move = move;
+            NetPack = netPack;
+            this.white = white;
+            this.black = black;
+            StatusCode = StatusCode.OK;
+        }
+
+        public override AsyncJob Run()
+        {
+            try {
+                if (move is not null) {
+                    NetPack.MoveId = GrpcRoutines.PostMove(NetPack.Client, NetPack.GameId, move);
+                }
+
+                CongoUser u;
+                do {
+
+                    Game = GrpcRoutines.GetLatestGame(NetPack.Client, NetPack.GameId);
+                    u = Game.ActivePlayer.IsWhite() ? white : black;
+
+                } while (!abandon && u is Net && !Game.HasEnded());
+
+                Moves = GrpcRoutines.GetMovesAfter(NetPack.Client, NetPack.GameId, NetPack.MoveId);
+            }
+            catch (RpcException ex) { StatusCode = ex.StatusCode; }
+
+            return this;
+        }
+
+        public override void Cancel() { }
+
+        public override void Abandon() => abandon = true;
     }
-    */
 }
