ec457b15baf2c0b5094ce4ee331a38a95f541842
Dmitry Zhukov <zhukov.dm.s@gmail.com>
2022-07-15 23:59:57 +0200

    specific cli grpc routine

---
+++ Congo.CLI/GrpcRoutinesCli.cs
@@ -0,0 +1,14 @@
+﻿using CongoClient = Congo.Server.CongoGrpc.CongoGrpcClient;
+
+namespace Congo.CLI
+{
+    internal static class GrpcRoutinesCli
+    {
+        public static long SyncMoves(CongoClient client, long gameId, long moveId, TextPresenter presenter)
+        {
+            var moves = GrpcRoutines.GetDbMovesAfter(client, gameId, moveId);
+            presenter.ShowNetworkTransitions(moves);
+            return moveId + moves.Count;
+        }
+    }
+}
